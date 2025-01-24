f58efc7ee89a847de37fe6b2cf5e7017adad5eff
Mujahid Islam <mbiplobe@gmail.com>
2025-01-24 18:22:46 +0600

    Sample function class add here. Using the following process easily understand how can you define a function.

---
+++ src/SecondModule/FunctionClass.kt
@@ -0,0 +1,12 @@
+package SecondModule
+
+class FunctionClass {
+    fun max(a : Int, b: Int): Int {
+        if (a> b)
+            return a
+        else
+            return b
+    }
+
+    fun max2(a: Int, b: Int) = if(a> b) a else b
+}
